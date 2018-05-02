6a85296f455823bf4f3603420377f4936ea10dad
Rajeev Kumar <rajeev.code@gmail.com>
2018-05-03 01:18:58 +0700

    changes

--- src/test/java/com/appium/test/Basics.java
+++ src/test/java/com/appium/test/Basics.java
@@ -14,13 +14,19 @@ public class Basics extends BaseTest
        AndroidDriver<AndroidElement> driver = capabilities();
        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-       /*tagname Syntax
-       // tagName[@attribute='value']
-        */
+        //xpath id className, androidUIautomator
+        /*    xpath Syntax
+         *    //tagName[@attribute='value']
 
-       driver.findElement(By.xpath("//android.widget.TextView[@text='Preference']")).click();
-       driver.findElement(By.xpath("//android.widget.TextView[@text='1. Preferences from XM']")).click();
-      // driver.quit();
+         *
+         */
+        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        driver.findElementByXPath("(//android.widget.TextView)[4]").click();
+        driver.findElementById("android:id/checkbox").click();
+        driver.findElementById("android:id/title").click();
+//        driver.findElementByXPath("(//android.widget.LinearLayout)[2]").click();
+//        driver.findElementByClassName("android.widget.EditText").sendKeys("hello");
+//        driver.findElementsByClassName("android.widget.Button").get(1).click();
 
 
     }
